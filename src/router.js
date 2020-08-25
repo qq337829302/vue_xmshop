ae9c9cfd86d4d1d6df267e8e035176ec314f0e23
zzq <337829302@qq.com>
2020-08-25 17:57:03 +0800

    插件引入、路由

---
+++ src/router.js
@@ -0,0 +1,18 @@
+import Vue from 'vue';
+import Router from 'vue-router';
+import Home from './pages/home';
+import Index from './pages/index';
+Vue.use(Router);
+
+export default new Router({
+    routes: [{
+        path: '/',
+        name: 'home',
+        component: Home,
+        children: [{
+            path: '/index',
+            name: 'index',
+            component: Index,
+        }, {}]
+    }, {}]
+})
